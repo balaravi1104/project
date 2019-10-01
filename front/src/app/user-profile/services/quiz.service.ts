04d0fd4506eb9cede2f635e303e473bfdebb384f
“balachitti” <bala.ravichandran@aspiresys.com>
2019-10-01 19:07:37 +0530

    edited

---
+++ front/src/app/user-profile/services/quiz.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class QuizService {
+
+  constructor(private http: HttpClient) { }
+
+  get(url: string) {
+    return this.http.get(url);
+  }
+
+  getAll() {
+    return [
+      { id: 'assets/data/javascript.json', name: 'JavaScript' },
+     { id: 'assets/data/designPatterns.json', name: 'Design Patterns' }
+    ];
+  }
+
+}
